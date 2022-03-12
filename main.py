c0fd5e3854ef672b7ce18a6a0ac9609c61017191
Baha <toktobekov1995@gmail.com>
2022-03-12 23:21:23 +0600

    start project

---
+++ main.py
@@ -0,0 +1,34 @@
+import logging
+
+from aiogram import (
+    Bot, Dispatcher,
+    executor, types
+)
+
+import config as cfg
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=cfg.API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+        This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\n I'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    """
+    handler
+    """
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
